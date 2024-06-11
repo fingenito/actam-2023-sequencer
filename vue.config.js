27dc7ebd459dc40622e23e64e255a30764995654
Flavio Ingenito <flavio.ingenito@mail.polimi.it>
2024-06-11 10:39:26 +0200

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Actam 2023 Sequencer</title>
+  <!-- Collegamento ai file CSS generati -->
+  <link rel="stylesheet" href="css/app.[hash].css">
+</head>
+<body>
+  <div id="q-app"></div>
+  <!-- Collegamento ai file JavaScript generati -->
+  <script src="js/app.[hash].js"></script>
+</body>
+</html>
